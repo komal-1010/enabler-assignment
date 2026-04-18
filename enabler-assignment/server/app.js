6cf11520910b7af7b41134bbaae1c914d9c7a72e
dhokkomal10 <dhokkomal10@gmail.com>
2026-04-18 11:48:12 +0000

    feat: Add and configure cors package

--- enabler-assignment/server/app.js
+++ enabler-assignment/server/app.js
@@ -3,7 +3,19 @@ const cors = require("cors");
 const uploadRoute = require("./routes/upload");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://69e36c6181d0b50009954b87--enabler-app.netlify.app",
+      "https://enabler-app.netlify.app/"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
 
 app.use("/api", uploadRoute);
 
