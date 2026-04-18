4c030c90b92d8dce3542d7d558edd6f88af63b45
dhokkomal10 <dhokkomal10@gmail.com>
2026-04-18 11:55:55 +0000

    feat: Temporarily allow all origins for debugging CORS

--- enabler-assignment/server/app.js
+++ enabler-assignment/server/app.js
@@ -3,17 +3,7 @@ const cors = require("cors");
 const uploadRoute = require("./routes/upload");
 
 const app = express();
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://69e36c6181d0b50009954b87--enabler-app.netlify.app",
-      "https://enabler-app.netlify.app/"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.options("*", cors());
 
