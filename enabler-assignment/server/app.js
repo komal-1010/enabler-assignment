24b5d7a0dc07916c15ae70fe9929c39db2f3a51f
dhokkomal10 <dhokkomal10@gmail.com>
2026-04-19 16:18:02 +0000

    Fix CORS issue

--- enabler-assignment/server/app.js
+++ enabler-assignment/server/app.js
@@ -3,9 +3,11 @@ const cors = require("cors");
 const uploadRoute = require("./routes/upload");
 
 const app = express();
-app.use(cors());
-
-app.options("*", cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use("/api", uploadRoute);
 
